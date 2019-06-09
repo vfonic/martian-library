89ee8e5233b0090ff1e175c227c3c2870cff9882
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-09 22:13:10 +0700

    Render our library

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,5 +2,11 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
+import Library from '../components/Library';
 
-render(<Provider>👻</Provider>, document.querySelector('#root'));
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+);
