291cc024ea36daeb98795e65df3ae217f6d55c43
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-09 22:09:10 +0700

    Setup Apollo Provider

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,1 +1,6 @@
-console.log('👻');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+
+render(<Provider>👻</Provider>, document.querySelector('#root'));
