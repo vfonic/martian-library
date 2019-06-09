e50971f81f63c325f8ae30c450bb8eca1731973c
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-09 21:52:24 +0700

    `rails g controller Library index --skip-routes`

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'library#index'
 end
