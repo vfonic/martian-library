b64c99d1f16b3c1f4d470d5d5b7ef8d09dca346b
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-20 12:44:31 +0700

    Add sign_in mutation

---
+++ app/graphql/mutations/base_mutation.rb
@@ -0,0 +1,11 @@
+# To sum up, in order to add a new mutation you need to complete the following steps:
+
+# Add a class implementing the mutation logic, which includes:
+# the input type definition (arguments);
+# the return type definition (fields);
+# the #resolve method.
+# Add a new entry to MutationType.
+module Mutations
+  class BaseMutation < GraphQL::Schema::Mutation
+  end
+end
