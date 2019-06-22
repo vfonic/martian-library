f33be849eb001ec9094730ab43cf30f9c994de5f
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-22 13:48:33 +0700

    Re-create Library component

--- app/javascript/components/Library/index.jsx
+++ app/javascript/components/Library/index.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import cs from './styles';
+import { LibraryQuery } from './operations.graphql';
 
-const LibraryQuery = gql`
-  {
-    items {
-      id
-      title
-      user {
-        email
-      }
-    }
-  }
-`;
+const Library = () => {
+  const [item, setItem] = useState(null);
 
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? 'loading...'
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
-            ))}
-      </div>
-    )}
-  </Query>
-);
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? 'loading...'
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+                <button
+                  key={id}
+                  className={cs.plate}
+                  onClick={() => setItem({ id, title, imageUrl, description })}
+                >
+                  <div className={cs.title}>{title}</div>
+                  {imageUrl && <img src={imageUrl} className={cs.image} />}
+                  {user ? (
+                    <div className={cs.user}>added by ${user.email}</div>
+                  ) : null}
+                </button>
+              ))}
+        </div>
+      )}
+    </Query>
+  );
+}
+
+export default Library;
