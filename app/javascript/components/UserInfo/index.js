967f7042bdc3e7f52cdac566a797b34c48e54e60
Viktor Fonic <viktor.fonic@gmail.com>
2019-06-22 13:19:55 +0700

    Add UserInfo component

--- app/javascript/components/UserInfo/index.js
+++ app/javascript/components/UserInfo/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import cs from './styles';
+import { Mutation, Query } from 'react-apollo';
+import { Me, SignMeIn } from './operations.graphql';
 
-const UserInfo = () => (
-  <div className={cs.userInfo}>👋</div>
-);
+const UserInfo = () => {
+  const emailInputRef = useRef(null);
+
+  return (
+    <div className={cs.panel}>
+      <Query query={Me}>
+        {({ data, loading }) => {
+          if (loading) return 'Loading...';
+          if (!data.me) {
+            return (
+              <Mutation
+                mutation={SignMeIn}
+                update={(cache, { data: { signIn } }) => {
+                  cache.writeQuery({
+                    query: Me,
+                    data: { me: signIn.user },
+                  });
+                }}
+              >
+                {(signIn, { loading: authenticating }) =>
+                  authenticating ? '...' : (
+                    <form onSubmit={e => {
+                      e.preventDefault();
+                      signIn({ variables: { email: emailInputRef.current.value } })
+                        .then(({ data: { signIn: { token } } }) => {
+                          if (token) localStorage.setItem('mlToken', token);
+                        });
+                    }}>
+                      <input
+                        ref={emailInputRef}
+                        type="email"
+                        className={cs.input}
+                        placeholder="your email"
+                      />
+                      <input type="submit" className={cs.button} value="Sign in" />
+                    </form>
+                  )
+                }
+              </Mutation>
+            );
+          }
+
+          const { fullName } = data.me;
+          return <div className={cs.info}>👾 {fullName}</div>;
+        }}
+      </Query>
+    </div>
+  );
+}
 
 export default UserInfo;
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library';
+import UserInfo from '../components/UserInfo';
 
 render(
   <Provider>
+    <UserInfo />
     <Library />
   </Provider>,
   document.querySelector('#root')
